a4ab00de61d0a68f24ce1bcee9b1b30e9982fb8e
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-14 00:25:44 +0600

    doctors-portal-client first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
+import ContactUs from './Pages/ContactUs/ContactUs';
+import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Reviews from './Pages/Reviews/Reviews';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" ">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/about' element={<Appointment />}></Route>
+        <Route path='/about' element={<Reviews />}></Route>
+        <Route path='/about' element={<ContactUs />}></Route>
+        <Route path='/about' element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
